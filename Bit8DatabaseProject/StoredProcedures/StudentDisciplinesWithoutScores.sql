3544f53b4b50ecbc6bf215be225c1ebd94db27ea
SimonaHristovska <simona.hristovska@outlook.com>
2021-02-08 09:38:26 +0100

    Modifying the addtional data calculation in the stored procedures.

--- Bit8DatabaseProject/StoredProcedures/StudentDisciplinesWithoutScores.sql
+++ Bit8DatabaseProject/StoredProcedures/StudentDisciplinesWithoutScores.sql
@@ -1,13 +1,19 @@
 ﻿CREATE PROCEDURE [dbo].[StudentDisciplinesWithoutScores]
 AS
-	SELECT s.[Name], 
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT s.[Name] AS StudentName, 
 		d.DisciplineName, 
-		sem.[Name] 
+		sem.[Name] AS SemesterName,
+		sc.Value
 	FROM Students s 
 	INNER JOIN StudentSemester ss ON ss.StudentID = s.Id
 	INNER JOIN Semesters sem ON sem.Id = ss.SemesterID
-	LEFT JOIN StudentDisciplines sd ON sd.StudentID = s.Id
-	LEFT JOIN Disciplines d ON d.ID = sd.DisciplineID
-	LEFT JOIN Scores sc ON sc.DisciplineID = d.ID
-	WHERE sc.[Value] IS NULL
+	INNER JOIN StudentDisciplines sd ON sd.StudentID = s.Id
+	INNER JOIN Disciplines d ON d.ID = sd.DisciplineID 
+	LEFT JOIN Scores sc ON sc.DisciplineID = d.ID AND sc.StudentID = s.Id
+	WHERE sc.[Value] IS NULL AND d.ID IS NOT NULL
 	ORDER BY s.[Name], sem.[Name]
+
+END
--- Bit8DatabaseProject/StoredProcedures/TopStudents.sql
+++ Bit8DatabaseProject/StoredProcedures/TopStudents.sql
@@ -2,15 +2,15 @@
 	@SemesterID INT = 0,
 	@MaxResults INT = 10
 AS
-
-	SELECT TOP(@MaxResults) s.Id AS StudentID,
+BEGIN
+    SET NOCOUNT ON;
+	SELECT DISTINCT TOP(@MaxResults) s.Id AS StudentID,
 		s.[Name] AS StudentName,
 		AVG(sc.[Value]) AS StudentAvgScore 
-	INTO #studentAvgScore
 	FROM Students s 
 	INNER JOIN StudentDisciplines sd ON sd.StudentID = s.Id
-	INNER JOIN Scores sc ON sc.DisciplineID = sd.DisciplineID
+	INNER JOIN Scores sc ON sc.DisciplineID = sd.DisciplineID AND sc.StudentID = s.Id
 	INNER JOIN SemesterDisciplines semD on semD.DisciplineID = sd.DisciplineID
-	GROUP BY s.Id, semD.SemesterID
+	GROUP BY s.Id, s.[Name], semD.SemesterID
 
-
+END
